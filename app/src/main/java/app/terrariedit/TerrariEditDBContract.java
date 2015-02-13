e2e6041b97b08e8632fa388b9f1463a0469588b5
Taylor Eernisse <tayloreernisse@gmail.com>
2015-02-13 10:15:41 -0500

    - Finished initial DB implementation; no testing done yet

--- app/src/main/java/app/terrariedit/TerrariEditDBContract.java
+++ app/src/main/java/app/terrariedit/TerrariEditDBContract.java
@@ -1,13 +1,16 @@
 package app.terrariedit;
 
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
 import android.provider.BaseColumns;
 
 /**
  * Created by teernisse on 2/10/2015.
  */
-public class TerrariEditReaderContract {
-    public TerrariEditReaderContract() {
-
+public class TerrariEditDBContract {
+    public TerrariEditDBContract() {
+        // here to prevent accidental object construction
     }
 
     private static final String TEXT_TYPE = " TEXT";
@@ -18,7 +21,8 @@ public class TerrariEditReaderContract {
                     Items.COLUMN_NAME_ITEM_NAME + TEXT_TYPE + COMMA_SEP +
                     Items.COLUMN_NAME_ITEM_ID + TEXT_TYPE +
                     " )";
-    private static final String SQL_DELETE_ITEMS = "";
+    private static final String SQL_DELETE_ITEMS =
+            "DEOP TABLE IF EXISTS " + Items.TABLE_NAME;
 
     private static final String SQL_CREATE_PREFIXES =
             "CREATE TABLE " + Prefixes.TABLE_NAME + " (" +
@@ -26,7 +30,8 @@ public class TerrariEditReaderContract {
                     Prefixes.COLUMN_NAME_PREFIX_NAME + TEXT_TYPE + COMMA_SEP +
                     Prefixes.COLUMN_NAME_PREFIX_ID + TEXT_TYPE +
                     " )";
-    private static final String SQL_DELETE_PREFIXES = "";
+    private static final String SQL_DELETE_PREFIXES =
+            "DEOP TABLE IF EXISTS " + Prefixes.TABLE_NAME;
 
     public static abstract class Items implements BaseColumns {
         public static final String TABLE_NAME = "items";
@@ -40,5 +45,29 @@ public class TerrariEditReaderContract {
         public static final String _ID = "id";
         public static final String COLUMN_NAME_PREFIX_ID = "prefix_id";
         public static final String COLUMN_NAME_PREFIX_NAME = "prefix_name";
+    }
+
+    public class TerrariEditDbHelper extends SQLiteOpenHelper {
+        public static final int DATABASE_VERSION = 1;
+        public static final String DATABASE_NAME = "TerrariEdit.db";
+
+        public TerrariEditDbHelper(Context context) {
+            super(context, DATABASE_NAME, null, DATABASE_VERSION);
+        }
+
+        public void onCreate(SQLiteDatabase db) {
+            db.execSQL(SQL_CREATE_ITEMS);
+            db.execSQL(SQL_CREATE_PREFIXES);
+        }
+
+        public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+            db.execSQL(SQL_DELETE_ITEMS);
+            db.execSQL(SQL_DELETE_PREFIXES);
+            onCreate(db);
+        }
+
+        public void onDowngrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+            onUpgrade(db, oldVersion, newVersion);
+        }
     }
 }
