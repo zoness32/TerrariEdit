389a7779f0f6e1006c214b51019325d833a5bf01
Taylor Eernisse <tayloreernisse@gmail.com>
2015-02-13 10:36:04 -0500

    - Began implementation of item and prefix file reader to populate the database

---
+++ app/src/main/java/app/terrariedit/CodeImportReader.java
@@ -0,0 +1,14 @@
+package app.terrariedit;
+
+/**
+ * Created by teernisse on 2/13/2015.
+ * This class is designed to be used only by me to get
+ * item and prefix names and ids into the app database.
+ * This will rarely be used - once for intial db
+ * population and only for edits thereafter.
+ */
+public class CodeImportReader {
+    public static void ImportItemList() {
+
+    }
+}
